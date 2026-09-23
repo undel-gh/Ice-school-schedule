7d758f125ee60df52a8e8b1f00cb8c0a06c8bff0
undel-gh <dnesterkin@gmail.com>
2026-09-23 15:38:22 +0300

    Add Django authentication routes

--- ice_school/urls.py
+++ ice_school/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("scheduling.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
