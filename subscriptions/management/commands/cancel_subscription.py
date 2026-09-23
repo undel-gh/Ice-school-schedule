dba86ad865ebc155ff5360550ef7a24d5fa64413
undel-gh <dnesterkin@gmail.com>
2026-09-23 18:14:45 +0300

    Wrap management command domain errors

--- subscriptions/management/commands/cancel_subscription.py
+++ subscriptions/management/commands/cancel_subscription.py
@@ -5,7 +5,7 @@ from uuid import UUID
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-from core.management import resolve_actor
+from core.management import command_errors, resolve_actor
 from subscriptions.services import cancel_subscription
 
 
@@ -16,6 +16,7 @@ class Command(BaseCommand):
         parser.add_argument("--subscription", required=True)
         parser.add_argument("--actor", required=True)
 
+    @command_errors
     def handle(self, *args, **options):
         try:
             subscription_id = UUID(options["subscription"])
