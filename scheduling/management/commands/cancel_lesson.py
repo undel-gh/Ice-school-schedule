3094e1c6483c2a4f4f7f1a60ff2e5cba8f6d83ce
undel-gh <dnesterkin@gmail.com>
2026-09-23 18:14:15 +0300

    Wrap management command domain errors

--- scheduling/management/commands/cancel_lesson.py
+++ scheduling/management/commands/cancel_lesson.py
@@ -5,7 +5,7 @@ from uuid import UUID
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-from core.management import resolve_actor
+from core.management import command_errors, resolve_actor
 from scheduling.models import Lesson
 from scheduling.services import cancel_lesson
 
@@ -18,6 +18,7 @@ class Command(BaseCommand):
         parser.add_argument("--reason", required=True, choices=Lesson.CancellationReason.values)
         parser.add_argument("--actor", required=True)
 
+    @command_errors
     def handle(self, *args, **options):
         try:
             lesson_id = UUID(options["lesson"])
