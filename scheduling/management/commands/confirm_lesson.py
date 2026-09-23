c9ad853f85c922f93aed43551ad5d94453a9fa26
undel-gh <dnesterkin@gmail.com>
2026-09-23 18:14:23 +0300

    Wrap management command domain errors

--- scheduling/management/commands/confirm_lesson.py
+++ scheduling/management/commands/confirm_lesson.py
@@ -5,7 +5,7 @@ from uuid import UUID
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-from core.management import resolve_actor
+from core.management import command_errors, resolve_actor
 from scheduling.services import confirm_lesson
 
 
@@ -16,6 +16,7 @@ class Command(BaseCommand):
         parser.add_argument("--lesson", required=True)
         parser.add_argument("--actor", required=True)
 
+    @command_errors
     def handle(self, *args, **options):
         try:
             lesson_id = UUID(options["lesson"])
